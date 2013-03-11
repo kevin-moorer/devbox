ebb8d643aaf6eda233399aa97111909064dd1d49
Kevin Moorer <kevin.moorer@aenetworks.com>
2013-03-11 16:19:25 -0400

    fixed memcache start script

--- files/startMemcached.sh
+++ files/startMemcached.sh
@@ -1,13 +1,12 @@
 # /bin/sh
 
 
-/usr/local/bin/memcached -u nobody -d -m 512 -l 127.0.0.1 -p 12001
-/usr/local/bin/memcached -u nobody -d -m 32 -l 127.0.0.1 -p 12002
-/usr/local/bin/memcached -u nobody -d -m 64 -l 127.0.0.1 -p 12003
-/usr/local/bin/memcached -u nobody -d -m 32 -l 127.0.0.1 -p 12004
-/usr/local/bin/memcached -u nobody -d -m 32 -l 127.0.0.1 -p 12005
-/usr/local/bin/memcached -u nobody -d -m 32 -l 127.0.0.1 -p 12006
-/usr/local/bin/memcached -u nobody -d -m 32 -l 127.0.0.1 -p 12007
-/usr/local/bin/memcached -u nobody -d -m 32 -l 127.0.0.1 -p 12008
-/usr/local/bin/memcached -u nobody -d -m 32 -l 127.0.0.1 -p 12009
-/usr/local/bin/memcached -u nobody -d -m 32 -l 127.0.0.1 -p 12010
+/usr/bin/memcached -u nobody -d -m 64 -l 127.0.0.1 -p 19111
+/usr/bin/memcached -u nobody -d -m 64 -l 127.0.0.1 -p 19112
+/usr/bin/memcached -u nobody -d -m 64 -l 127.0.0.1 -p 19113
+/usr/bin/memcached -u nobody -d -m 64 -l 127.0.0.1 -p 19114
+/usr/bin/memcached -u nobody -d -m 64 -l 127.0.0.1 -p 19115
+/usr/bin/memcached -u nobody -d -m 64 -l 127.0.0.1 -p 19116
+/usr/bin/memcached -u nobody -d -m 64 -l 127.0.0.1 -p 19117
+/usr/bin/memcached -u nobody -d -m 64 -l 127.0.0.1 -p 19118
+
